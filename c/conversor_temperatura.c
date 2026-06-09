35a09f8b5559477aadf2ca8867551d9736da973e
eliza <elizafernandamribeiro2009@gmail.com>
2026-06-09 12:41:07 -0300

    Delete c directory

--- c/conversor_temperatura.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float tempc, tempf;
-    printf("Digite a temperatura em graus celsius:");
-    scanf("%f", &tempc);
-    tempf = tempc * 1.8 + 32;
-    printf("temperatura em fahrenheit igual á %f", tempf);
-}
